b6e03a4c33cdf78445e49ba0af61bdeb1a5bc33b
Nassande1 <Muyimbwad3@gmail.com>
2026-03-23 19:52:56 +0300

    Create Database.php file

---
+++ config/Database.php
@@ -0,0 +1,25 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "secure_api_db";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function dbConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo json_encode(['error' => 'Connection failed: ' . $exception->getMessage()]);
+            exit;
+        }
+        return $this->conn;
+    }
+}
+?>
